9ad2fec96c92799796575250c2bf1910958d9922
tahawy111 <amertahawy111@gmail.com>
2023-07-11 02:41:50 +0300

    Finish website 4

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -8,7 +8,7 @@ interface SkillCardProps {
 
 export default function SkillCard({ image, name }: SkillCardProps) {
   return (
-    <div className="bg-slate-900 w-40 h-w-40 px-6 py-4 text-center text-white p-5 rounded-2xl hover:bg-slate-950 transition-all hover:scale-105 hover:shadow-2xl">
+    <div className="bg-slate-900 w-40 h-w-40 px-6 py-4 text-center text-white p-5 rounded-2xl hover:bg-slate-800 transition-all hover:scale-105 hover:shadow-2xl">
       <div className="justify-center flex my-3 w-10 h-10 mx-auto">
       <Image width={50} height={50} src={image} alt="Skill Image"/>
       </div>
