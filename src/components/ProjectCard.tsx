2f172c3e81ff749e837c23c44f59cf2e6d718f38
tahawy111 <amertahawy111@gmail.com>
2023-08-04 01:14:18 +0300

    Fix some design in Project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -26,20 +26,28 @@ export default function ProjectCard({ project }: ProjectCardProps) {
           height={9999}
           alt="Project image"
         />
-        <div className="duration-300 group-hover:translate-y-0 -bottom-[50%] group-hover:bottom-0 absolute bg-white/100 h-52 flex justify-between flex-col pb-3">
+        <div
+          className={cn(
+            "duration-300 group-hover:translate-y-0 absolute bg-white/100 group-hover:bottom-0 flex justify-between flex-col pb-3",
+            project.description.length > 100
+              ? "h-64 -bottom-[64%]"
+              : "h-52 -bottom-[50%]"
+          )}
+        >
           <h1 className="text-xl text-center font-bold bg-neutral-900 text-white py-3">
-            <span className="group-hover:hidden">
-              {project.title.length > 25
-                ? project.title.slice(0, 25) + "..."
+              <span className="hidden md:block">{project.title}</span>
+            <span className="group-hover:hidden md:hidden">
+              {project.title.length > 20
+                ? project.title.slice(0, 20) + "..."
                 : project.title}
             </span>
-            <span className="hidden group-hover:block">{project.title}</span>
+            <span className="hidden group-hover:md:hidden group-hover:block">{project.title}</span>
           </h1>
           <p className="py-3 px-1">{project.description}</p>
           <div
             className={cn(
-              "flex justify-between px-1",
-              project.description.length > 140 && "-translate-y-3"
+              "flex justify-between px-1 bg-white/100",
+              project.description.length > 100 && "-translate-y-3"
             )}
           >
             <a
